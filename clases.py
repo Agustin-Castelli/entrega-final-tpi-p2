76201c6b5ec71897259c2ef950b07788b5086fa5
Agustin-Castelli <141937150+Agustin-Castelli@users.noreply.github.com>
2023-10-31 23:26:27 -0300

    Add files via upload

---
+++ clases.py
@@ -0,0 +1,174 @@
+from generadores import generar_contrasenia
+from abc import ABC
+from datetime import datetime
+
+class Usuario(ABC):
+    def __init__(self, nombre: str, apellido: str, email: str, contrasenia: str):
+        self._nombre = nombre
+        self._apellido = apellido
+        self._email = email
+        self._contrasenia = contrasenia
+        
+    def __str__(self) -> str:
+       return f"Nombre: {self._nombre}, Apellido: {self._apellido}, Email: {self._email}"
+    
+    def get_nombre(self): #metodo protegido que retornará el atributo protegido _nombre
+        return self._nombre
+    
+    def get_apellido(self): 
+        return self._apellido
+    
+    def get_email(self): 
+        return self._email
+    
+    def get_contrasenia(self):
+        return self._contrasenia
+    
+    def validar_credenciales(self, email, contrasenia)-> bool:
+        if self._email == email and self._contrasenia == contrasenia:
+            return True
+        else:
+            return False
+
+
+class Estudiante(Usuario):
+    estudiantes = []
+    def __init__(self, _legajo: int, _anio_inscripcion_carrera: int, nombre, apellido, email, contrasenia):
+        super().__init__(nombre, apellido, email, contrasenia)
+        self.__legajo = _legajo
+        self.__anio_inscripcion_carrera = _anio_inscripcion_carrera
+        self._mis_cursos = []
+    def __str__(self) -> str:
+       return f"Legajo: {self.__legajo}, Año: {self.__anio_inscripcion_carrera}"
+   
+    def matricular_en_curso(self):
+        lista_cursos = Curso.cursos
+        for indice, curso in enumerate(lista_cursos):
+            print(f"{indice + 1}. {curso.get_nombre()}")
+        indice_curso = int(input("Ingrese el índice de un curso para matricularse: "))
+        for indice, curso in enumerate(lista_cursos):
+            if (indice + 1) == indice_curso:
+                if curso in self._mis_cursos:
+                    print("Usted ya se encuentra matriculado en esta materia.")
+                else:
+                    while True:
+                        contrasenia_ingresada = str(input("Ingrese la clave de matriculación: "))
+                        if contrasenia_ingresada == curso.get_contrasenia_matriculacion():
+                            self._mis_cursos.append(curso)
+                            print("Se ha matriculado exitosamente.")
+                            break
+                        else:
+                            print("Clave incorrecta.")
+                        
+    def desmatricular_curso(self):
+        lista_cursos = Curso.cursos
+        for indice, curso in enumerate(lista_cursos):
+            print(f"{indice + 1}. {curso.get_nombre()}")
+        indice_curso = int(input("Ingrese el índice de un curso para desmatricularse: "))
+        for indice, curso in enumerate(lista_cursos):
+            if (indice + 1) == indice_curso:
+                if curso in self._mis_cursos:
+                    self._mis_cursos.remove(curso)
+                    print("Se ha desmatriculado con éxito del curso.")
+                else:
+                   print("El índice ingresado no pertenece a ningún curso.")
+                            
+    def get_legajo(self):
+        return self.__legajo
+    def get_anio_inscripcion_carrera(self):
+        return self.__anio_inscripcion_carrera
+    def get_mis_cursos(self):
+        return self._mis_cursos
+    
+    
+class Profesor(Usuario):
+    profesores = []
+    def __init__(self, _titulo: str, _anio_egreso: int, nombre, apellido, email, contrasenia):
+        super().__init__(nombre, apellido, email, contrasenia)
+        self.__titulo = _titulo
+        self.__anio_egreso = _anio_egreso
+        self._mis_cursos = []
+    def __str__(self) -> str:
+        return f"Titulo: {self.__titulo}, Año Egreso: {self.__anio_egreso}"
+    
+    def dictar_curso(self):
+        nuevo_curso = str(input("Ingrese el nombre del curso: "))
+        nueva_contrasenia = generar_contrasenia(6)
+        curso_creado = Curso(nuevo_curso, nueva_contrasenia) 
+        Curso.cursos.append(curso_creado) 
+        self._mis_cursos.append(curso_creado)
+        print(f"Se ha creado el siguiente curso exitosamente.")
+        print("Nombre: ", nuevo_curso)
+        print("Contraseña: ", nueva_contrasenia)
+        print("Código del curso:", curso_creado.get_codigo())
+        
+    def get_titulo(self):
+        return self.__titulo
+    def get_anio_egreso(self):
+        return self.__anio_egreso
+    def get_mis_cursos(self):
+        return self._mis_cursos
+    
+    
+class Curso():
+    cursos = []
+    __prox_cod = 1
+    
+    def __init__(self, __nombre: str, __contrasenia_matriculacion: str):
+        self.__nombre = __nombre
+        __codigo =Curso.__prox_cod
+        self.__codigo = __codigo
+        self.__contrasenia_matriculacion = __contrasenia_matriculacion
+        Curso.__prox_cod += 1
+        self.archivos = []
+        self.cant_archivos = 0
+        
+    def __str__(self) -> str:
+        return f"Codigo:{self.__codigo}, Nombre del curso: {self.__nombre}, Contraseña de matriculación: {self.__contrasenia_matriculacion}"
+
+    @classmethod
+    def __generar_contrasenia(cls):
+        return cls.__generar_contrasenia(6)
+    
+    def nuevo_archivo(self):
+        nombre = str(input("Ingresa el nombre del archivo:"))
+        fecha = datetime.now()
+        formato = str("pdf")
+        nuevo_archivo = Archivo(nombre, fecha, formato)
+        self.archivos.append(nuevo_archivo)
+        
+    def get_nombre(self):
+        return self.__nombre
+    def get_contrasenia_matriculacion(self):
+        return self.__contrasenia_matriculacion
+    def get_codigo(self):
+        return self.__codigo
+    
+class Carrera():
+    def __init__(self, __nombre: str, __cant_anios: int):
+        self.__nombre = __nombre
+        self.__cant_anios = __cant_anios
+    def __str__(self) -> str:
+        return f"Nombre del curso: {self.__nombre}, Duración: {self.__cant_anios} años"
+    
+    def get_cantidad_materias() -> int:
+        pass                          #completar------------------------------------------------------
+    def get_nombre(self):
+        return self.__nombre
+    def get_cant_anios(self):
+        return self.__cant_anios
+    
+class Archivo():
+    def __init__(self, __nombre: str, __fecha: datetime, __formato: str):
+        self.__nombre = __nombre
+        self.__fecha= __fecha
+        self.__formato= __formato
+    def __srt__(self):
+        return f"Nombre del archivo: {self.__nombre}, Fecha: {self.__fecha}, Formato: {self.__formato}"    
+    
+    def get_nombre(self):
+        return self.__nombre
+    def get_fecha(self):
+        return self.__fecha
+    def get_formato(self):
+        return self.__formato
---
+++ main.py
@@ -0,0 +1,140 @@
+from clases import *
+from funciones import registrar_profesor
+# ----------------- BLOQUE PRINCIPAL -----------------
+
+# ALUMNOS, PROFESORES Y ARCHIVOS CARGADOS DE ARRANQUE:
+estudiante1 = Estudiante("1000", 2023, "Lucas", "Ochoa", "lucasochoa@gmail.com", "1234")
+Estudiante.estudiantes.append(estudiante1)
+estudiante2 = Estudiante("1001", 2023, "Jorge", "Maslaton", "jorgemaslaton@gmail.com", "asd")
+Estudiante.estudiantes.append(estudiante2)
+
+
+profesor1 = Profesor("Ingeniero en Sistemas", 2017, "Abelardo", "Varela", "turbotomi@gmail.com", "hola123")
+Profesor.profesores.append(profesor1)
+profesor2 = Profesor("Profesorado en Computacion", 2010, "Bryan", "Shelby", "lsrp@gmail.com", "piyicones")
+Profesor.profesores.append(profesor2)
+
+# -------------------MENU PRINCIPAL ----------------#
+
+opcion = 0
+
+while opcion != 4:
+    print("Menú:")
+    print("1. Ingresar como alumno")
+    print("2. Ingresar como profesor")
+    print("3. Ver cursos")
+    print("4. Salir")
+
+    opcion = int(input("Selecciona una opción (1/2/3/4): "))
+    if opcion == 1:
+        print("Usted está por ingresar como alumno")
+        email = str(input("Ingrese el mail: "))
+        contrasenia = str(input("Ingrese su contraseña: "))
+        email_encontrado = False
+        contrasenia_encontrada = False
+        for e in Estudiante.estudiantes:
+            if email == e._email:
+                if e.validar_credenciales(email, contrasenia) == True:
+                    print("Verificado")
+                    contrasenia_encontrada = True
+            # -------------------SUB-MENU ALUMNOS ----------------#
+                    sub_opcion_alumno = 0
+                    while sub_opcion_alumno !=4:
+                        print("1. Matricularse a un curso")
+                        print("2. Desmatricularse a un curso")
+                        print("3. Ver curso")
+                        print("4. Volver al menú principal")
+                        sub_opcion_alumno = int(input("Selecciona una opción (1/2/3) \n"))
+                        if sub_opcion_alumno == 1:
+                            e.matricular_en_curso()
+                        elif sub_opcion_alumno == 2:
+                            e.desmatricular_curso()
+                        elif sub_opcion_alumno == 3:    
+                            for indice, curso in enumerate(e.get_mis_cursos()):
+                                print(f"{indice + 1}. {curso.get_nombre()}") 
+                            indice_curso = int(input("Ingrese el índice de un curso para ver sus archivos: "))
+                            for indice, curso in enumerate(e.get_mis_cursos()):
+                                if (indice + 1) == indice_curso:
+                                    print(f"Nombre del curso:{curso.get_nombre()}")# aca faltaría llamar a la lista de archivos (de ese curso en especifico)
+                                    for archivo in curso.archivos:
+                                        print(f"{archivo.get_nombre()}.{archivo.get_formato()}")
+                                else:
+                                    print("El índice ingresado no coincide con ningun curso.")
+                        elif sub_opcion_alumno == 4:         
+                            print("Volviendo al menú principal...")
+                            break
+                        else:
+                            print("Ingrese una opción válida")
+                if contrasenia_encontrada == False:
+                    "Error: contraseña incorrecta."
+                    email_encontrado = True
+        if email_encontrado == False:
+            print("El email ingresado no existe, debe darse de alta en Alumnado.")
+            
+        
+    elif opcion == 2:
+        print("Usted está por ingresar como profesor")
+        _email = str(input("Ingrese el mail: "))
+        _contrasenia = str(input("Ingrese su contraseña: "))
+        email_encontrado = False
+        contrasenia_encontrada = False
+        for p in Profesor.profesores:
+            if _email == p._email:
+                if p.validar_credenciales(_email, _contrasenia) == True:
+                    print("Verificado")
+                    contrasenia_encontrada = True
+                    # -------------------SUB-MENU PROFESOR ----------------#
+                    sub_opcion_profesor = 0
+                    cant_archivos = 0
+                    while sub_opcion_profesor != 3:
+                        print("1. Dictar curso")
+                        print("2. Ver curso")
+                        print("3. Volver al menú principal")
+                        sub_opcion_profesor = int(input("Selecciona una opción (1/2/3) \n"))
+                        if sub_opcion_profesor == 1:
+                            p.dictar_curso()
+                        elif sub_opcion_profesor == 2:
+                            print(f"Listado de cursos dictados por {p._nombre}:")
+                            for indice, c in enumerate(p.get_mis_cursos()):
+                                print(f"{indice + 1}. {c.get_nombre()}")
+                            indice_curso = int(input("Ingrese el índice de un curso para ver sus datos: "))
+                            for indice, c in enumerate(p.get_mis_cursos()):
+                                if (indice + 1) == indice_curso:
+                                    if c in p.get_mis_cursos():
+                                        print(f"Nombre del curso: {c.get_nombre()} \n Contraseña: {c.get_contrasenia_matriculacion()} \n Código: {c.get_codigo()} \n Cantidad de archivos: {cant_archivos}") #faltaria agregar llamada a cant de archivos
+                                    #REVISAR ----------------------------
+                                    while True:
+                                        agregar_adjunto = int (input("¿Desea agregar un archivo adjunto? \n 1. Si \n 2.No \n"))
+                                        if agregar_adjunto == 1:
+                                            c.nuevo_archivo()
+                                            c.cant_archivos += 1
+                                            print(f"Nombre del curso: {c.get_nombre()} \n Contraseña: {c.get_contrasenia_matriculacion()} \n Código: {c.get_codigo()} \n Cantidad de archivos: {c.cant_archivos}")
+                                        elif agregar_adjunto == 2:
+                                            break
+                                        else:
+                                            print("Seleccione una opción válida")
+                        elif sub_opcion_profesor == 3:
+                            print("Volviendo al menú principal...")
+                            break
+                        else:
+                            print("Ingrese una opción válida")
+                if contrasenia_encontrada == False:
+                    "Error: contraseña incorrecta."
+                email_encontrado = True
+        if email_encontrado == False:
+            print("El email ingresado no existe.")
+            dar_alta = str(input("Ingrese la contraseña para darse de alta como profesor: "))   
+            if dar_alta == "admin":
+                registrar_profesor()
+                print("Profesor registrado exitosamente.")
+                
+    elif opcion == 3:
+        print("Cursos disponibles:")
+        lista_cursos_ordenada = sorted(Curso.cursos, key=lambda x: x.get_nombre)
+        for curso in lista_cursos_ordenada:
+            print("-"*10)
+            print(f"Nombre: {curso.get_nombre()}, Carrera: Tecnicatura Universitaria en Programación")
+        
+    else:
+        print("Saliendo del sistema...")
+        break
